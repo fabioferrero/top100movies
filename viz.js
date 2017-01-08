88f2c4b0246a84f80eaed462228c9b1ad3fde622
Fabio Ferrero <fabio.ferrero@icloud.com>
2017-01-08 18:11:43 +0100

    Eliminate Force Directed Graph and put random position into the canvas. WARNING: important issue in tsv data files (faulty raws moved into notes.txt)

--- viz.js
+++ viz.js
@@ -6,78 +6,135 @@ var nodes = [];
 // Load movies data
 d3.tsv('data/movies.tsv', function(error, data) {
     if(error) throw error;
+    d3.tsv('data/metadata.tsv', function(error, metadata) {
+        if(error) throw error;
 
-    var numberOfFilms = data.length;
+        var numberOfFilms = data.length;
+        console.log(data.length + ' ' + metadata.length);
 
-    // Create the similarity matrix
-    var similarity = new Array(numberOfFilms);
-    for (var i = 0; i < numberOfFilms; i++) {
-        similarity[i] = new Array(numberOfFilms);
-    }
-
-    // compute some statistic while loading
-    data.forEach(function(d) {
-        var score = 0;
-        var critics = [];
-        for(var i=0; i<177; i++) {
-            critic = 'c'+d3.format('03')(i+1);
-            rank = d[critic];
-            if(rank > 0) {
-                score += 11 - rank;
-                critics.push(critic);
-            }
+        // Create the similarity matrix
+        var similarity = new Array(numberOfFilms);
+        for (var i = 0; i < numberOfFilms; i++) {
+            similarity[i] = new Array(numberOfFilms);
         }
-        d.score = score;
-        d.critics = critics;
-    });
 
-    data = data.filter(function(d) { return d.score > 20; });
-    numberOfFilms = data.length;
-
-    // compute the similarity matrix
-    for (var i = 0; i < numberOfFilms; i++) {
-        similarity[i][i] = 1;
-        nodes.push({'id': data[i].title, 'score': data[i].score})
-        for (var j = i+1; j < numberOfFilms; j++) {
-            list1 = data[i]['critics'];
-            list2 = data[j]['critics'];
-            commonCritics = 0;
-            for (var x = 0, y = 0; x < list1.length && y < list2.length;) {
-                if (list1[x] > list2[y]) {
-                    y++;
-                } else if (list1[x] < list2[y]) {
-                    x++;
+        // compute some statistic while loading
+        data.forEach(function(d) {
+            var score = 0;
+            var critics = [];
+            var index = data.indexOf(d);
+            for(var i=0; i<177; i++) {
+                critic = 'c'+d3.format('03')(i+1);
+                rank = d[critic];
+                if(rank > 0) {
+                    score += 11 - rank;
+                    critics.push(critic);
                 } else {
-                    x++;
-                    y++;
-                    commonCritics++;
+                    delete d[critic];
                 }
             }
-            similarity[i][j] = commonCritics / Math.max(list1.length, list2.length);
-            similarity[j][i] = similarity[i][j];
-            if (similarity[i][j] != 0) {
-                links.push({'source': data[i].title, 'target': data[j].title, 'value': similarity[i][j]});
-            } /*else {
-                links.push({'source': data[i].title, 'target': data[j].title, 'value': 0.01});
-            }*/
+            d.score = score;
+            d.critics = critics;
+            var elem = metadata[index];
+            d.poster = elem['Poster'];
+        });
+
+        data = data.filter(function(d) { return d.score > 20; });
+        numberOfFilms = data.length;
+
+        var totalScore = 0;
+
+        // compute the similarity matrix
+        for (var i = 0; i < numberOfFilms; i++) {
+            similarity[i][i] = 1;
+            nodes.push({'id': data[i].title, 'score': data[i].score})
+            totalScore += data[i].score;
+            for (var j = i+1; j < numberOfFilms; j++) {
+                list1 = data[i]['critics'];
+                list2 = data[j]['critics'];
+                commonCritics = 0;
+                for (var x = 0, y = 0; x < list1.length && y < list2.length;) {
+                    if (list1[x] > list2[y]) {
+                        y++;
+                    } else if (list1[x] < list2[y]) {
+                        x++;
+                    } else {
+                        x++;
+                        y++;
+                        commonCritics++;
+                    }
+                }
+                similarity[i][j] = commonCritics / Math.max(list1.length, list2.length);
+                similarity[j][i] = similarity[i][j];
+                if (similarity[i][j] != 0) {
+                    links.push({'source': data[i].title, 'target': data[j].title, 'value': similarity[i][j]});
+                } /*else {
+                    links.push({'source': data[i].title, 'target': data[j].title, 'value': 0.01});
+                }*/
+            }
         }
-    }
 
-    console.log(nodes.length + ' ' + links.length);
+        var svg = d3.select('svg'),
+        width = svg.attr('width'),
+        height = svg.attr('height');
 
-    var svg = d3.select('svg'),
-    width = svg.attr('width'),
-    height = svg.attr('height');
+        // Compute statistics
+        var totalArea = width*height/2;
 
-    var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink()
-            .id(function(d) { return d.id; })
-            .distance(function(d) { return 20/d.value; })
-        )
-        .force("charge", d3.forceManyBody().strength(-40))
-        .force("center", d3.forceCenter(width / 2, height / 2));
+        var scores = data.map((d) => { return d.score; });
 
-    var link = svg.append('g')
+        var maxScore = Math.max.apply(null, scores);
+        var minScore = Math.min.apply(null, scores);
+
+        
+
+        data.forEach((d) => {
+
+            var movieArea = Math.floor(totalArea * d.score / totalScore);
+            var movieWidth = 6 * Math.sqrt(movieArea / 54);
+            var movieHeight = 9 * Math.sqrt(movieArea / 54);
+
+            var randomX = Math.floor(Math.random() * width);
+            var randomY = Math.floor(Math.random() * height);
+
+            // Check for boundaries
+            if (randomX + movieWidth >= width) {
+                randomX -= width - randomX + movieWidth + 1;
+            }
+            if (randomX <= 0) {
+                randomX = 1;
+            }
+            if (randomY + movieHeight >= height) {
+                randomY -= height - randomY + movieHeight + 1;
+            }
+            if (randomY <= 0) {
+                randomY = 1;
+            }
+
+            svg.append("defs")
+               .append("pattern")
+               .attr("id", d.id)
+               .attr('patternUnits', 'userSpaceOnUse')
+               .attr('width', movieWidth)
+               .attr('height', movieHeight)
+               .append("image")
+               .attr("xlink:href", d.poster)
+               .attr('width', movieWidth)
+               .attr('height', movieHeight);
+
+            var movie = svg.append('rect')
+                .attr('x', randomX)
+                .attr('y', randomY)
+                .attr('width', movieWidth)
+                .attr('height', movieHeight)
+                .attr("fill", "url(#" + d.id + ")")
+                .attr('class', 'movie');
+        })
+    });
+
+    console.log('stop');
+
+    /*var link = svg.append('g')
         .attr('class', 'links')
         .selectAll('line')
         .data(links)
@@ -94,25 +151,7 @@ d3.tsv('data/movies.tsv', function(error, data) {
         //.call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended))
         ;
 
-    simulation.nodes(nodes)
-        .on('tick', ticked);
-
-    simulation.force('link')
-        .links(links);
-
-    function ticked() {
-        link
-            .attr('x1', function(d) { return d.source.x; })
-            .attr('y1', function(d) { return d.source.y; })
-            .attr('x2', function(d) { return d.target.x; })
-            .attr('y2', function(d) { return d.target.y; });
-
-        node
-            .attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
-    }
-
-    /*body.selectAll('.movie')
+    body.selectAll('.movie')
         .data(data
             .filter(function(d) { return d.score > 20; })
             .sort(function(a, b) { return b.score - a.score; })
